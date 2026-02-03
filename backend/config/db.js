285fa3f6f65f4022c33c09a23979600a73f67628
rioatchu-max <rioatchu@gmail.com>
2026-02-03 18:14:04 +0530

    commited

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,15 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ovenstory");
+    await mongoose.connect(process.env.MONGO_URL);
 
-    console.log("MongoDB Connected Successfully");
+    console.log("✅ MongoDB Atlas Connected Successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error("❌ MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
 
-export default connectDB;  
+export default connectDB;
+
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
 import connectDB from "./config/db.js";
 
 import menuCardRoutes from "./routes/menuCards.js";
@@ -7,6 +9,7 @@ import menuBuilderRoutes from "./routes/menuBuilders.js";
 import videoRoutes from "./routes/videos.js";
 import authRoutes from "./routes/auth.js";
 
+dotenv.config(); // 🔥 VERY IMPORTANT
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,16 +20,16 @@ app.use(express.json());
 // MongoDB
 connectDB();
 
-// ROUTES 👇🔥
+// ROUTES
 app.use("/api/menucards", menuCardRoutes);
 app.use("/api/menubuilder", menuBuilderRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
 
 app.get("/", (req, res) => {
-  res.send("OvenStory Backend Running");
+  res.send("OvenStory Backend Running 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });

173a7327c2a67c58decb514f8fae6db5b4819ef8
rioatchu-max <rioatchu@gmail.com>
2026-02-04 12:56:34 +0530

    commited

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 import menuCardRoutes from "./routes/menuCards.js";
@@ -7,6 +8,7 @@ import menuBuilderRoutes from "./routes/menuBuilders.js";
 import videoRoutes from "./routes/videos.js";
 import authRoutes from "./routes/auth.js";
 
+dotenv.config(); // 🔥 MUST
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,19 +16,19 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB
+// 🔥 MongoDB connect
 connectDB();
 
-// ROUTES 👇🔥
+// ROUTES
 app.use("/api/menucards", menuCardRoutes);
 app.use("/api/menubuilder", menuBuilderRoutes);
-app.use("/api/auth", authRoutes);
 app.use("/api/videos", videoRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("OvenStory Backend Running");
+  res.send("OvenStory Backend Running 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });

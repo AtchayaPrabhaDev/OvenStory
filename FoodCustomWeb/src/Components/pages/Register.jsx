977ffdd8687c228a78e75183a84e1adab449be67
rioatchu-max <rioatchu@gmail.com>
2026-02-05 12:33:38 +0530

    commited

--- FoodCustomWeb/src/Components/pages/Register.jsx
+++ FoodCustomWeb/src/Components/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { registerUser } from "../services/api";
+import { registerUser } from "../../services/api";
 
 export default function Register() {
   const navigate = useNavigate();
